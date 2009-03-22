b032987eb4b485bba6469d182f54725961c3707a
Adam Vandenberg <flangy@gmail.com>
2009-03-21 17:43:58 -0700

    Prompt for username/password in delicious backup script.

--- bin/backup-delicious.sh
+++ bin/backup-delicious.sh
@@ -1,1 +1,6 @@
-curl --user username:password -o ~/Docs/delicious.xml -O 'https://api.del.icio.us/v1/posts/all'
+## Back up delicious.com links
+# Ref: http://rentzsch.com/notes/backingUpDelicious
+read -p "Usename: " myusername
+read -s -p "Password: " mypassword
+printf '\nBacking up delicious bookmarks for user: %s\n' $myusername
+curl --user $myusername:$mypassword -o ~/Docs/delicious.xml -O 'https://api.del.icio.us/v1/posts/all'
