b5be7e28b879e4aa30e198d1604657845a069af7
Adam Vandenberg <flangy@gmail.com>
2009-07-31 17:49:40 -0700

    Update link script.

--- bin/linkme.sh
+++ bin/linkme.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
 function relink() {
-  rm -i $1
+  if [-e $1] ; then rm -i $1; fi
   ln -sn $2 $1
 }
 
 cd ~
-    relink .bash_profile ~/.dotfiles/.bash_profile
-    relink .bashrc ~/.dotfiles/.bashrc
-    relink .gitconfig ~/.dotfiles/.gitconfig
-    relink bin ~/.dotfiles/bin
-popd
+relink .bash_profile ~/.dotfiles/.bash_profile
+relink .bashrc ~/.dotfiles/.bashrc
+relink .gitconfig ~/.dotfiles/.gitconfig
+relink bin ~/.dotfiles/bin
