76e7754616807291edcb1079e1151824cf86427b
Adam Vandenberg <flangy@gmail.com>
2010-07-25 16:55:52 -0700

    Rename Bash dotfiles and update installer script.
    I want to be able to do "mate ." in this git repo and get most
    of the files in the editor. Dot-files are filtered by TextMate by
    default, so rather than mess with TM's settings, just prefix the
    Bash files and change the installer script to symlink correctly.

--- bin/install-dotfiles.sh
+++ bin/install-dotfiles.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 
 function relink() {
-  if [-e $1] ; then rm -i $1; fi
-  ln -sn $2 $1
+  if [[ -h "$1" ]]; then
+    # Symbolic link? Then recreate.
+    rm "$1"
+    ln -sn "$2" "$1"
+  elif [[ ! -e "$1" ]]; then
+    ln -sn "$2" "$1"
+  else
+    echo "$1 exists as a real file, skipping."
+  fi
 }
 
 cd ~
-relink .bash_profile ~/.dotfiles/.bash_profile
-relink .bashrc ~/.dotfiles/.bashrc
+relink .bash_profile ~/.dotfiles/dot.bash_profile
+relink .bashrc ~/.dotfiles/dot.bashrc
 relink .gitconfig ~/.dotfiles/.gitconfig
 relink bin ~/.dotfiles/bin
