ae4c83a874c17fd8c1b5b4f204d7f87d11c40bba
Adam Vandenberg <flangy@gmail.com>
2016-03-29 20:55:21 -0700

    this doesn't work anymore

--- bin/backup-delicious.sh
+++
@@ -1,7 +0,0 @@
-## Back up delicious.com links
-# Ref: http://rentzsch.com/notes/backingUpDelicious
-read -p "Usename: " myusername
-read -s -p "Password: " mypassword
-printf '\nBacking up delicious bookmarks for user: %s\n' $myusername
-mkdir -p ~/Documents/backups
-curl --user $myusername:$mypassword -o ~/Documents/backups/$myusername-delicious.xml -O 'https://api.del.icio.us/v1/posts/all'
